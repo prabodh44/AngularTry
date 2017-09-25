e53f6b8f6554a4a3445ae3e3c13781659eff3243
Prabodh Tuladhar <prabodh@iths-Macintosh.local>
2017-09-25 18:06:40 +0545

    Completed the code for the header-ccmpoent

--- components/bodyComponent.js
+++ components/bodyComponent.js
@@ -1,7 +1,6 @@
 
 app.component('bodyComponent', {
     templateUrl: 'components/body.html',
-    controllerAs:'$bodyCtrl',
     controller: function(){
         this.name = "prabodh",
         this.alert = function(){
--- components/headerComponent.js
+++ components/headerComponent.js
@@ -1,6 +1,19 @@
 
 app.component('headerComponent', {
-    templateUrl: 'components/header.html'
+    templateUrl: 'components/header.html',
+    controller: function() {
+        this.setView = function(view){
+            this.view = view,
+            this.onViewChange({$event: {view: view}})
+        }
+    },
+    binding: {
+        view: '<',
+        onViewChange: '&'
+    }
+    
 });
+
+
 
 // ale
