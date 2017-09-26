0d73da998f42da90c9302e4d2eaeb8cd59e3b2be
Prabodh Tuladhar <prabodh@iths-Macintosh.local>
2017-09-26 16:33:31 +0545

    Deleted the previous folder ststructure. All the logic created in the app.js file

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
 var app = angular.module('myApp', []);
+app.controller('RootController', function(){
+    this.view = 'Home'
+});
 
+
+app.component('headerComponent',{
+    template: `<h4> Header component </h4>
+        <a class = "btn btn-default" ng-class="{'btn-primary' : $ctrl.view === 'Home'}" ng-click="$ctrl.setView('Home')">Home</a>
+        <a class = "btn btn-default" ng-class="{'btn-primary' : $ctrl.view === 'About'}" ng-click="$ctrl.setView('About')">About</a>
+        `,
+    controller: function(){
+        this.setView = function(view){
+            this.view = view;
+            this.onViewChange({header : {view : view}});
+            //this.onTest();
+            //console.log(this.onViewChange({$event: {view : view}}));
+        }
+        
+    },
+    bindings:{
+        view: '<',
+        onViewChange: '&',
+        onTest : '&'
+    },
+    
+});
+
+app.component('bodyComponent', {
+    template: `<p> {{ $ctrl.view }} has been clicked</p>`,
+    bindings: {
+        view: '<'
+    }
+});
+
+///////////////////////////////////////Practice/////////////////////////////////////////
